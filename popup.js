7a18e974c9f741fe4e5c3710db65a05ca0e7dae7
Anton Kuznetsov <lukarek@Antons-MacBook-Pro.local>
2021-06-16 23:57:53 +0400

    Попап закрывается самостоятельно

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 var switchersStates = {};
 
 function copyToClipboard(str) {
-	document.oncopy = function(event) {
+	document.oncopy = function (event) {
 		event.clipboardData.setData('text/plain', str);
 		event.preventDefault();
 	};
@@ -11,18 +11,22 @@ function copyToClipboard(str) {
 function initOptionSwitcher(imgNode, domainOption, globalOption, srcValues) {
 	switchersStates[domainOption] = domainOption in localStorage ? +localStorage[domainOption] : (localStorage[globalOption] ? 1 : 0);
 	imgNode.src = srcValues[switchersStates[domainOption]];
-	imgNode.onclick = function() {
+	imgNode.onclick = function () {
 		switchersStates[domainOption] = +!switchersStates[domainOption];
 		localStorage[domainOption] = switchersStates[domainOption] ? 1 : '';
 		imgNode.src = srcValues[switchersStates[domainOption]];
 	};
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 	var errorsNode = document.getElementById('errors');
 	var copyNode = document.getElementById('copy');
 	var clearNode = document.getElementById('clear');
 	var settings = document.getElementById('settings_pop');
+
+	errorsNode.addEventListener('click', () => {
+
+	})
 
 	var iconNode = document.getElementById('showIcon');
 	iconNode.title = 'Show error notification icon on ' + request.host;
@@ -38,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		'img/enabled.png'
 	]);
 
-	if(!request.errors) {
+	if (!request.errors) {
 		errorsNode.innerHTML = '<p style="padding: 20px">There are no errors on this page :)</p>';
 		copyNode.remove();
 		clearNode.remove();
@@ -46,42 +50,46 @@ document.addEventListener('DOMContentLoaded', function() {
 	else {
 
 
-			let leo = request.errors;
-			let test = new RegExp('<br/>','g');
-			let mass = leo.replace(test,'').split(',').reverse().slice(1).reverse();
+		let leo = request.errors;
+		let test = new RegExp('<br/>', 'g');
+		let mass = leo.replace(test, '').split(',').reverse().slice(1).reverse();
+		setTimeout(() => {
+			clearNode.click();
+		}, 3000)
 
 
-			 mass.forEach((item, i) => {
-				errorsNode.innerHTML = `<p>${i+1} - ${item}</p>`;
-			 });
-			settings.style= 'display:none;';
+		mass.forEach((item, i) => {
+			errorsNode.innerHTML = `<p>${i + 1} - ${item}</p>`;
+		});
+		settings.style = 'display:none;';
 
-		clearNode.onclick = function() {
+		clearNode.onclick = function () {
 			closePopup(isIFrame);
 		};
 
-		copyNode.onclick = function() {
+		copyNode.onclick = function () {
 			var isWindows = navigator.appVersion.indexOf('Windows') != -1;
 			copyToClipboard(request.errors.replace(/<br\/>/g, isWindows ? '\r\n' : '\n').replace(/<.*?>/g, ''));
 			closePopup();
 		};
 	}
 
-	window.addEventListener('message', function(event) {
-		if(typeof event.data == 'object' && event.data._reloadPopup) {
+	window.addEventListener('message', function (event) {
+		if (typeof event.data == 'object' && event.data._reloadPopup) {
 			request = parseUrl(event.data.url);
 
 			let leo = request.errors;
-			let test = new RegExp('<br/>','g');
-			let mass = leo.replace(test,'').split(',').reverse().slice(1).reverse();
+			let test = new RegExp('<br/>', 'g');
+			let mass = leo.replace(test, '').split(',').reverse().slice(1).reverse();
 
 
-			 mass.forEach((item, i) => {
-				  errorsNode.innerHTML = `<p>${i} - ${item}</p>`;
-				  console.log(`${i+1} - ${item}`);
-			 })
+			mass.forEach((item, i) => {
+				errorsNode.innerHTML = `<p>${i} - ${item}</p>`;
+				console.log(`${i + 1} - ${item}`);
+			})
 
 			console.log(mass.reverse().slice(1).reverse());
+
 
 
 
